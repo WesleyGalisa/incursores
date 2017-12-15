4c5800356451b56ad1c6e1b54b52f742b3448b19
WesleyGalisa <wesleygalisa@gmail.com>
2017-12-15 09:29:52 -0200

    em andamento

---
+++ javascript/slide.js
@@ -0,0 +1,39 @@
+var slide1 = document.getElementById("slid1");
+
+var slide2 = document.getElementById("slid2");
+
+var slide3 = document.getElementById("slid3");
+
+var slide4 = document.getElementById("slid4");
+
+function slider(){
+    slide1.style.display="block";
+    slide2.style.display="none";
+    slide3.style.display="none";
+    slide4.style.display="none";
+    setTimeout("slider2()" ,3000);
+}
+
+function slider2(){
+    slide1.style.display="none";
+    slide2.style.display="block";
+    slide3.style.display="none";
+    slide4.style.display="none";
+    setTimeout("slider3()" ,3000);
+}
+
+function slider3(){
+    slide1.style.display="none";
+    slide2.style.display="none";
+    slide3.style.display="block";
+    slide4.style.display="none";
+    setTimeout("slider4()" ,3000);
+}
+
+function slider4(){
+    slide1.style.display="none";
+    slide2.style.display="none";
+    slide3.style.display="none";
+    slide4.style.display="block";
+    setTimeout("slider()" ,3000);
+}
