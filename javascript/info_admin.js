ec30b2818a1d91988cd0d31ca8725b12df57aed1
WesleyGalisa <wesleygalisa@gmail.com>
2017-12-22 23:19:52 -0200

    em andamento

---
+++ javascript/info_admin.js
@@ -0,0 +1,93 @@
+var admin = false;
+
+var admin1 = document.getElementById("admin1");
+var admin2 = document.getElementById("admin2");
+var admin3 = document.getElementById("admin3");
+var admin4 = document.getElementById("admin4");
+var admin5 = document.getElementById("admin5");
+var admin6 = document.getElementById("admin6");
+var admin7 = document.getElementById("admin7");
+var admin8 = document.getElementById("admin8");
+var admin9 = document.getElementById("admin9");
+var admin10 = document.getElementById("admin10");
+var admin11 = document.getElementById("admin11");
+var admin12 = document.getElementById("admin12");
+var admin13 = document.getElementById("admin13");
+var admin14 = document.getElementById("admin14");
+var admin15 = document.getElementById("admin15");
+
+function clicker(){
+    admin = !admin;
+    admin1.style.display = admin ? "block" : "none";
+};
+
+function clicker1(){
+    admin = !admin;
+    admin2.style.display = admin ? "block" : "none";
+};
+
+function clicker2(){
+    admin = !admin;
+    admin3.style.display = admin ? "block" : "none";
+};
+
+function clicker3(){
+    admin = !admin;
+    admin4.style.display = admin ? "block" : "none";
+};
+
+function clicker4(){
+    admin = !admin;
+    admin5.style.display = admin ? "block" : "none";
+};
+
+function clicker5(){
+    admin = !admin;
+    admin6.style.display = admin ? "block" : "none";
+};
+
+function clicker6(){
+    admin = !admin;
+    admin7.style.display = admin ? "block" : "none";
+};
+
+function clicker7(){
+    admin = !admin;
+    admin8.style.display = admin ? "block" : "none";
+};
+
+function clicker8(){
+    admin = !admin;
+    admin9.style.display = admin ? "block" : "none";
+};
+
+function clicker9(){
+    admin = !admin;
+    admin10.style.display = admin ? "block" : "none";
+};
+
+function clicker10(){
+    admin = !admin;
+    admin11.style.display = admin ? "block" : "none";
+};
+
+function clicker11(){
+    admin = !admin;
+    admin12.style.display = admin ? "block" : "none";
+};
+
+function clicker12(){
+    admin = !admin;
+    admin13.style.display = admin ? "block" : "none";
+};
+
+function clicker13(){
+    admin = !admin;
+    admin14.style.display = admin ? "block" : "none";
+};
+
+function clicker14(){
+    admin = !admin;
+    admin15.style.display = admin ? "block" : "none";
+};
+
